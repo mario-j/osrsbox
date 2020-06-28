017de23b5a1f6f42a8ac5e2c2f1ebdc3927035a0
Mario Jordan <mjordanbartolo@gmail.com>
2020-06-28 13:33:21 -0500

    restructure project

--- app.py
+++ app.py
@@ -1,13 +1,56 @@
-from flask import Flask, render_template
-from osrsbox import items_api
+  
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/todo.db'
 
-@app.route("/")
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200))
+    complete = db.Column(db.Boolean)
+
+@app.route('/')
 def index():
-    return render_template('index.html')
+    incomplete = Todo.query.filter_by(complete=False).all()
+    complete = Todo.query.filter_by(complete=True).all()
 
-if __name__ == "__main__":
+    return render_template('index.html', incomplete=incomplete, complete=complete)
+
+@app.route('/add', methods=['POST'])
+def add():
+    todo = Todo(text=request.form['todoitem'], complete=False)
+    db.session.add(todo)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+@app.route('/search', methods=['POST'])
+def search():
+
+    search = request.form.get('search')
+    todo_list = db.session.query(Todo).filter(Todo.text.like("%" + search + "%")).all()
+    return render_template('search.html', todo_list=todo_list )
+
+@app.route('/complete/<id>')
+def complete(id):
+
+    todo = Todo.query.filter_by(id=int(id)).first()
+    todo.complete = True
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+@app.route('/delete/<id>')
+def delete(id):
+
+    todo = Todo.query.filter_by(id=int(id)).delete()
+    db.session.commit()
+
+    return redirect('/')
+
+if __name__ == '__main__':
     app.run(debug=True)
-
