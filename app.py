759f8337eaeae2e2f57bd738f5a588096de78c5a
Mario Jordan <mjordanbartolo@gmail.com>
2020-06-27 21:08:22 -0500

    add json query for single item

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
+from osrsbox import items_api
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def index():
     return render_template('index.html')
-
 
 if __name__ == "__main__":
     app.run(debug=True)
